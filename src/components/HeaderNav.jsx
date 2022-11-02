c555f6dc19876edeb0478e31877a33f7a12951cf
Barbosaadev <silvag1212@gmail.com>
2022-11-01 21:22:16 -0300

    Sign In and Sign Up Containers

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
+
 export const HeaderNav = () => {
   return (
     <nav className="hidden lg:flex list-none">
       <li>
-        <a href="#" className="text-white">
+        <Link to="/" className="text-white px-4">
           Meus favoritos
-        </a>
+        </Link>
+      </li>
+      <li>
+        <Link to="/Signin" className="text-white px-4">
+          Sign In
+        </Link>
+      </li>
+      <li>
+        <Link to="/Signup" className="text-white px-4">
+          Sign Up
+        </Link>
       </li>
     </nav>
   );
